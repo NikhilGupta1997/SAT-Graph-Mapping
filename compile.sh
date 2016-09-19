2d73cf818a420748b6e31f2925073cfdbd32275a
Shardul Tripathi <shardultripathi@Sharduls-MacBook-Pro.local>
2016-09-19 14:38:19 +0530

    Improved compile.sh
     Committer: Shardul Tripathi <shardultripathi@Sharduls-MacBook-Pro.local>

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ## Compile our two program files
-g++ -o create main1.cpp
-g++ -o map  main2.cpp
+g++ -std=c++11 -Ofast -march=native -o create main1.cpp
+g++ -std=c++11 -Ofast -march=native -o map  main2.cpp
